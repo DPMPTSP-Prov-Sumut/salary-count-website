c03d519eb6cbaa597263a41a049e709ccbaa0244
Lexx0406 <gintingalex06@gmail.com>
2025-03-04 13:53:00 +0700

    domisili

--- controller/admin/karyawan.php
+++ controller/admin/karyawan.php
@@ -4,8 +4,11 @@ include '../koneksi.php';
 $hapus_id = $_GET['hapus_id'] ?? null;
 
 if (!empty($hapus_id)) {
-  $query3 = "DELETE FROM pegawai WHERE id_pegawai = '$hapus_id'";
-  mysqli_query($db, $query3);
+  $query3 = "DELETE FROM pegawai WHERE id_pegawai = ?";
+  $stmt = mysqli_prepare($db, $query3);
+  mysqli_stmt_bind_param($stmt, "s", $hapus_id);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
 
   echo '<script type="text/javascript">';
   echo 'window.location= "../../view/admin/karyawan.php";';
@@ -35,6 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   } elseif (!isset($_POST['id_pegawai']) && isset($_POST['nama'])) { // Add New Data
     $nama = $_POST['nama'] ?? '';
     $jabatan = $_POST['jabatan'] ?? '';
+    $alamat_domisili = $_POST['domisili'] ?? '';
     $mulai_kerja = $_POST['mulai_kerja'] ?? '';
     if (isset($_FILES['file_pkwt']) && $_FILES['file_pkwt']['error'] == 0) {
       $fileContent = addslashes(file_get_contents($_FILES['file_pkwt']['tmp_name']));
@@ -44,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $fileContent = '';
     }
 
-    $query2 = "INSERT INTO pegawai SET nama = '$nama', jabatan = '$jabatan', mulai_kerja = '$mulai_kerja', file_pkwt = '$fileContent', file_pkwt_name = '$originalName'";
+    $query2 = "INSERT INTO pegawai SET nama = '$nama', jabatan = '$jabatan', alamat_domisili = '$alamat_domisili' , mulai_kerja = '$mulai_kerja', file_pkwt = '$fileContent', file_pkwt_name = '$originalName'";
     mysqli_query($db, $query2);
 
     echo '<script type="text/javascript">';
